f1d98bb71d19bccbfad35d5ada11b6dbe248d7f9
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-07-19 20:00:06 +0600

    Implemented invoice page

--- src/Pages/InvoicePage/InvoicePage.jsx
+++ src/Pages/InvoicePage/InvoicePage.jsx
@@ -4,32 +4,40 @@ import { useQuery } from '@tanstack/react-query';
 import UseAxiosSecureAPI from '../../CustomHooks/UseAxiosSecureAPI';
 import siteLogo from '../../assets/siteLogo.png';
 import { IoCloudDownload } from "react-icons/io5";
-import { usePDF } from 'react-to-pdf';
+// import { useReactToPrint } from "react-to-print";
+// import { useRef } from "react";
 
 const InvoicePage = () => {
-  const currentDate = new Date().toLocaleDateString();
-  const { user } = useContext(AuthContext);
-  const axiosApi = UseAxiosSecureAPI();
-  const { toPDF, targetRef } = usePDF({ filename: 'invoice.pdf' });
+    const currentDate = new Date().toLocaleDateString();
+    const { user } = useContext(AuthContext);
+    const axiosApi = UseAxiosSecureAPI();
+    //   const { toPDF, targetRef } = usePDF({ filename: 'invoice.pdf' });
 
-  const { data: purchasedItems = [] } = useQuery({
-    queryKey: ["cart", user?.email],
-    enabled: !!user?.email,
-    queryFn: async () => {
-      const res = await axiosApi.get(`/myCart?email=${user.email}`);
-      return res.data;
-    },
-  });
+        
+    // const contentRef = useRef<HTMLDivElement>(null);
+    // const reactToPrintFn = useReactToPrint({
+    //     content: () => contentRef.current,
+    // });
+
+    const { data: purchasedItems = [] } = useQuery({
+        queryKey: ["cart", user?.email],
+        enabled: !!user?.email,
+        queryFn: async () => {
+        const res = await axiosApi.get(`/myCart?email=${user.email}`);
+        return res.data;
+        },
+    });
 
   const grandTotal = purchasedItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
   return (
     <div className="max-w-4xl mx-auto p-8 shadow-lg mt-10 rounded-2xl"
-         style={{ backgroundColor: 'rgb(152, 161, 188)', color: 'rgb(8, 12, 59)' }}>
+     style={{ color: 'rgb(8, 12, 59)' }}>
       {/* Download Button */}
       <div className="flex justify-end mb-4">
         <button
-          onClick={() => toPDF()}
+            // onClick={() => toPDF()}
+            // onClick={reactToPrintFn}
           className="cursor-pointer font-bold text-2xl flex gap-3 justify-center items-center px-6 py-2 rounded-xl transition"
           style={{
             backgroundColor: 'rgb(152, 161, 188)',
@@ -41,11 +49,11 @@ const InvoicePage = () => {
         </button>
       </div>
 
-      <div ref={targetRef}>
+      <div>
         {/* Header */}
         <div className="flex justify-between items-center border-b pb-6">
           <div>
-            <img src={siteLogo} alt="Website Logo" className="h-16" />
+            <img src={siteLogo} alt="Website Logo" className="h-20" />
             <h2 className="text-xl font-bold mt-2">Aroggo Medicine Store</h2>
           </div>
           <div className="text-right">
@@ -57,26 +65,26 @@ const InvoicePage = () => {
 
         {/* User Info */}
         <div className="mt-6 border-b pb-4">
-          <h4 className="text-lg font-semibold mb-1">Billed To:</h4>
-          <p>{user?.name || "N/A"}</p>
-          <p>{user?.email}</p>
+          <h4 className="text-2xl font-semibold mb-1">Billed To:</h4>
+          <p className='text-xl'>{user?.displayName || "N/A"}</p>
+          <p className='text-xl'>{user?.email}</p>
         </div>
 
         {/* Medicine Table */}
         <div className="mt-6">
-          <table className="w-full table-auto border-collapse">
+          <table className="w-full table table-zebra text-2xl table-auto border-collapse">
             <thead>
-              <tr style={{ backgroundColor: 'rgb(222, 211, 196)', color: 'rgb(8, 12, 59)' }}>
-                <th className="p-3">Medicine</th>
-                <th className="p-3">Company</th>
-                <th className="p-3">Quantity</th>
-                <th className="p-3">Unit Price</th>
-                <th className="p-3">Total</th>
+              <tr style={{ backgroundColor: 'rgb(152, 161, 188)', color: 'rgb(8, 12, 59)' }}>
+                <th className="text-2xl p-3">Medicine</th>
+                <th className="text-2xl p-3">Company</th>
+                <th className="text-2xl p-3">Quantity</th>
+                <th className="text-2xl p-3">Unit Price</th>
+                <th className="text-2xl p-3">Total</th>
               </tr>
             </thead>
             <tbody>
               {purchasedItems.map((item, idx) => (
-                <tr key={idx} className="border-t" style={{ backgroundColor: idx % 2 === 0 ? 'white' : 'rgb(244, 235, 211)' }}>
+                <tr key={idx} className="border-t">
                   <td className="p-3">{item.name}</td>
                   <td className="p-3">{item.company}</td>
                   <td className="p-3">{item.quantity}</td>
