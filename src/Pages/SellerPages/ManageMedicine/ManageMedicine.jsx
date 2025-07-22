5695066e7f8fcb8fc705d304f49e80645c92d6af
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-07-22 17:09:08 +0600

    Added a dynamic categories feature in Manage Medicine page

--- src/Pages/SellerPages/ManageMedicine/ManageMedicine.jsx
+++ src/Pages/SellerPages/ManageMedicine/ManageMedicine.jsx
@@ -11,23 +11,23 @@ const ManageMedicine = () => {
   const queryClient = useQueryClient();
 
   const [showModal, setShowModal] = useState(false);
-  const [medicineImage , setMedicineImage] = useState("");
+  const [medicineImage, setMedicineImage] = useState("");
 
   const axiosApi = UseAxiosSecureAPI();
-
-  
 
   const handlePhotoUpload = async (e) => {
     const image = e.target.files[0];
 
     const formData = new FormData();
-    formData.append("image" , image);
+    formData.append("image", image);
 
-    const imageUploadUrl = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_image_key_api}`;
+    const imageUploadUrl = `https://api.imgbb.com/1/upload?key=${
+      import.meta.env.VITE_image_key_api
+    }`;
 
-    const res = await axiosApi.post(imageUploadUrl,formData);
+    const res = await axiosApi.post(imageUploadUrl, formData);
     setMedicineImage(res.data.data.url);
-  }
+  };
 
   const handleAddMedicine = async (e) => {
     e.preventDefault();
@@ -46,44 +46,61 @@ const ManageMedicine = () => {
       discount: parseFloat(form.discount.value || 0),
       added_at: new Date().toISOString(),
     };
-    console.log(medicineInfo)
-    const res = await axiosApi.post("/medicines" , medicineInfo)
-    console.log(res.data)
-    if(res.data.insertedId){
+    console.log(medicineInfo);
+    const res = await axiosApi.post("/medicines", medicineInfo);
+    console.log(res.data);
+    if (res.data.insertedId) {
       toast.success("New medicine added successfully!");
 
-      await queryClient.invalidateQueries({queryKey: ["myMedicines",user?.email]})
+      await queryClient.invalidateQueries({
+        queryKey: ["myMedicines", user?.email],
+      });
       form.reset();
       setMedicineImage("");
       showModal(false);
+    } else {
+      toast.error(
+        "There are some error adding this medicine. Please try again."
+      );
     }
-    else{
-      toast.error("There are some error adding this medicine. Please try again.")
-    }
-
   };
 
-
   // to load data per email
-  const { data: medicines = [], isLoading} = useQuery({
+  const { data: medicines = [], isLoading } = useQuery({
     queryKey: ["myMedicines", user?.email],
     enabled: !!user?.email,
     queryFn: async () => {
       const res = await axiosApi.get(`/medicines/email?email=${user?.email}`);
       return res.data;
-    }
+    },
   });
 
-  if(isLoading){
-    return <div className="flex justify-center items-center h-screen">
-              <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
-            </div>
+  // to get all categories
+  const { data: categories = [] } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const res = await axiosApi.get("/categories");
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
+  console.log(categories);
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin border-blue-500"></div>
+      </div>
+    );
   }
 
   return (
     <div className="p-6 mt-10">
       <div className="flex justify-between items-center mb-6">
-        <h1 className="text-4xl font-bold text-[#080c3b]">Manage Your Medicines</h1>
+        <h1 className="text-4xl font-bold text-[#080c3b]">
+          Manage Your Medicines
+        </h1>
         <button
           onClick={() => setShowModal(true)}
           className="btn bg-[#98A1BC] text-[#080c3b] text-2xl hover:bg-[#7f89a4]"
@@ -94,7 +111,8 @@ const ManageMedicine = () => {
 
       {medicines.length === 0 ? (
         <p className="text-xl text-gray-600">
-          You haven't added any medicines yet. Click “Add Medicine” to get started.
+          You haven't added any medicines yet. Click “Add Medicine” to get
+          started.
         </p>
       ) : (
         <div className="overflow-x-auto mt-20">
@@ -124,7 +142,11 @@ const ManageMedicine = () => {
                   <td>৳{med.price.toFixed(2)}</td>
                   <td>{med.discount.toLocaleString()}%</td>
                   <td>
-                    <img src={med.image} alt="med" className="w-12 h-12 rounded" />
+                    <img
+                      src={med.image}
+                      alt="med"
+                      className="w-12 h-12 rounded"
+                    />
                   </td>
                 </tr>
               ))}
@@ -139,20 +161,39 @@ const ManageMedicine = () => {
           <div className="mt-10  modal-box bg-[#555879] text-[#F4EBD3] max-w-2xl">
             <form onSubmit={handleAddMedicine} className="space-y-4 text-lg">
               <h2 className="text-3xl font-semibold mb-4">Add New Medicine</h2>
-              <input name="name" required placeholder="Medicine Name" className="text-xl input input-bordered w-full text-black" />
-              <textarea name="description" required placeholder="Short Description" className="text-xl textarea textarea-bordered w-full text-black" />
-              <input type="file" required className="text-black text-xl file-input file-input-bordered w-full" onChange={handlePhotoUpload} />
-              <select name="category" required className="text-xl select select-bordered w-full text-black">
+              <input
+                name="name"
+                required
+                placeholder="Medicine Name"
+                className="text-xl input input-bordered w-full text-black"
+              />
+              <textarea
+                name="description"
+                required
+                placeholder="Short Description"
+                className="text-xl textarea textarea-bordered w-full text-black"
+              />
+              <input
+                type="file"
+                required
+                className="text-black text-xl file-input file-input-bordered w-full"
+                onChange={handlePhotoUpload}
+              />
+              <select
+                name="category"
+                required
+                className="text-xl select select-bordered w-full text-black"
+              >
                 <option defaultValue={true}>Select Category</option>
-                <option>Tablet</option>
-                <option>Syrup</option>
-                <option>Injection</option>
-                <option>Capsule</option>
-                <option>Ointment</option>
-                <option>Drop</option>
-                <option>Inhaler</option>
+                {categories.map((cat, index) => 
+                  <option key={index}>{cat.categoryName}</option>
+                )}
               </select>
-              <select name="generic" required className="text-xl select select-bordered w-full text-black">
+              <select
+                name="generic"
+                required
+                className="text-xl select select-bordered w-full text-black"
+              >
                 <option defaultValue={true}>Select Generic</option>
                 <option>Paracetamol</option>
                 <option>Ciprofloxacin</option>
@@ -162,7 +203,11 @@ const ManageMedicine = () => {
                 <option>Montelukast</option>
                 <option>Amlodipine</option>
               </select>
-              <select name="company" required className="text-xl select select-bordered w-full text-black">
+              <select
+                name="company"
+                required
+                className="text-xl select select-bordered w-full text-black"
+              >
                 <option defaultValue={true}>Select Company</option>
                 <option>Square</option>
                 <option>Beximco</option>
@@ -173,18 +218,56 @@ const ManageMedicine = () => {
                 <option>Aristopharma Ltd</option>
               </select>
 
-              <label htmlFor="unit" className="font-bold text-2xl">Item Mass Unit</label>
-              <input name="unit" type="number" required placeholder="Item Mass Unit(ML or MG)" className="text-xl input input-bordered w-full text-black" />
+              <label htmlFor="unit" className="font-bold text-2xl">
+                Item Mass Unit
+              </label>
+              <input
+                name="unit"
+                type="number"
+                required
+                placeholder="Item Mass Unit(ML or MG)"
+                className="text-xl input input-bordered w-full text-black"
+              />
 
-              <label htmlFor="price" className="font-bold text-2xl">Per Unit Price</label>
-              <input name="price" type="number" min="0" required placeholder="Per Unit Price" className="text-xl input input-bordered w-full text-black" />
+              <label htmlFor="price" className="font-bold text-2xl">
+                Per Unit Price
+              </label>
+              <input
+                name="price"
+                type="number"
+                min="0"
+                required
+                placeholder="Per Unit Price"
+                className="text-xl input input-bordered w-full text-black"
+              />
 
-              <label htmlFor="discount" className="font-bold text-2xl">Add Discount(if any)</label>
-              <input name="discount" type="number" defaultValue={0} min="0" max="50" placeholder="Discount (%)" className="text-xl input input-bordered w-full text-black" />
+              <label htmlFor="discount" className="font-bold text-2xl">
+                Add Discount(if any)
+              </label>
+              <input
+                name="discount"
+                type="number"
+                defaultValue={0}
+                min="0"
+                max="50"
+                placeholder="Discount (%)"
+                className="text-xl input input-bordered w-full text-black"
+              />
 
               <div className="modal-action">
-                <button type="submit" className="btn bg-[#98A1BC] text-[#080c3b] text-xl hover:bg-gray-300 border-none">Add Medicine</button>
-                <button onClick={() => setShowModal(false)} type="button" className="btn bg-[#98A1BC] text-[#080c3b] text-xl hover:bg-gray-300 border-none">Close</button>
+                <button
+                  type="submit"
+                  className="btn bg-[#98A1BC] text-[#080c3b] text-xl hover:bg-gray-300 border-none"
+                >
+                  Add Medicine
+                </button>
+                <button
+                  onClick={() => setShowModal(false)}
+                  type="button"
+                  className="btn bg-[#98A1BC] text-[#080c3b] text-xl hover:bg-gray-300 border-none"
+                >
+                  Close
+                </button>
               </div>
             </form>
           </div>
@@ -195,3 +278,9 @@ const ManageMedicine = () => {
 };
 
 export default ManageMedicine;
+
+// {
+//                   categories.map((cat,index)=>{
+//                     <option key={index}>{cat.categoryName}</option>
+//                   })
+//                 }
