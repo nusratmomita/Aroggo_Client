78ec44dd77add428c180a7b95f5ec21099d0fa5d
nusratmomita <108608844+nusratmomita@users.noreply.github.com>
2025-07-28 00:28:05 +0600

    Created interceptors to protect APIs

--- src/CustomHooks/UseAxiosSecureAPI.jsx
+++ src/CustomHooks/UseAxiosSecureAPI.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useContext } from 'react';
+import { useNavigate } from 'react-router';
+import { AuthContext } from "../Authentication/AuthContext";
 
 const axiosAPI = axios.create({
     baseURL: `http://localhost:3000`
@@ -7,6 +9,43 @@ const axiosAPI = axios.create({
 
 
 const UseAxiosSecureAPI = () => {
+    const { user, handleLogout } = useContext(AuthContext);
+    const navigate = useNavigate();
+    // console.log(user?.accessToken)
+
+    axiosAPI.interceptors.request.use(
+    (config) => {
+        config.headers.Authorization = `Bearer ${user?.accessToken}`;
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+    );
+
+    axiosAPI.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+        console.log("Inside response interceptor", error);
+        const errorStatus = error.status;
+
+        if (errorStatus === 403) {
+            navigate("/forbiddenRoute");
+        } 
+        else if (errorStatus === 401) {
+            handleLogout()
+            .then(() => {
+                navigate("/login");
+            })
+            .catch(() => {});
+        }
+        }
+
+    )
+
+
     return axiosAPI
 };
 
